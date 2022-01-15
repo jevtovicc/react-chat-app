bc5c90bd0f5fcdcbf94178595c938910ebe5b19d
jevtovicc <jevtovic99@yahoo.com>
2022-01-15 13:36:05 +0100

    Fix npm start script path

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,28 +1,16 @@
 import { Request, Response } from "express";
-import User from '../models/User'
+import prisma from "../../prisma/client";
 
-const users: User[] = [
-    {
-        firstName: 'Filip',
-        lastName: 'Jevtovic',
-        username: 'jevtovic99',
-        password: 'sifra123',
-        photoUrl: 'https://www.w3schools.com/howto/img_avatar.png',
-    },
-    {
-        firstName: 'Pera',
-        lastName: 'Peric',
-        username: 'pera123',
-        password: 'pera123',
-        photoUrl: 'https://www.w3schools.com/howto/img_avatar.png',
-    }
-]
-
-export function login(req: Request<{}, {}, { username: string, password: string }>, res: Response) {
+export async function login(req: Request<{}, {}, { username: string, password: string }>, res: Response) {
     const { username, password } = req.body;
-    const user = users.find(u => u.username === username && u.password === password);
 
-    if (!user) {
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username,
+        }
+    });
+
+    if (!user || user.password !== password) {
         res
             .status(401)
             .json({ message: "Invalid credentials" })
