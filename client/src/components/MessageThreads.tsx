fc523ef8d1b81defed5946b461d47a1b14519e12
jevtovicc <jevtovic99@yahoo.com>
2022-01-16 16:44:09 +0100

    Sorting message threads by most recent

--- client/src/components/MessageThreads.tsx
+++ client/src/components/MessageThreads.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from "../store/hooks"
 import MessageThreadPreview from "./MessageThreadPreview"
 import { MessageThread } from "../types/types";
 import { fetchMessageThreads } from "../store/features/MessagesSlice";
+import dayjs from "dayjs";
 
 function MessageThreadPreviews() {
     const messageThreads = useAppSelector(state => state.messages.messageThreads)
@@ -16,12 +17,20 @@ function MessageThreadPreviews() {
     return (
         <div className="bg-gray-900 text-gray-100">
             <ul>
-                {messageThreads.map(mt => (
-                    <MessageThreadPreview
-                        key={mt.id}
-                        messageThread={mt}
-                    />
-                ))}
+                {/* Sort message threads by most recent */}
+                {[...messageThreads]
+                    .sort((mt1, mt2) => {
+                        // if there is no last message, take creation date of message thread instead
+                        const mt1Time = dayjs((mt1.messages[mt1.messages.length - 1]?.sentAt || mt1.createdAt)?.toString());
+                        const mt2Time = dayjs((mt2.messages[mt2.messages.length - 1]?.sentAt || mt2.createdAt)?.toString());
+                        return -1 * mt1Time.diff(mt2Time)
+                    })
+                    .map(mt => (
+                        <MessageThreadPreview
+                            key={mt.id}
+                            messageThread={mt}
+                        />
+                    ))}
             </ul>
         </div>
     )
