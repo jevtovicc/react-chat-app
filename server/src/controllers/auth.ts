55063f65e805f5b646b8689f3772d1393d061dea
jevtovicc <jevtovic99@yahoo.com>
2022-01-22 20:16:56 +0100

    Password hashing via bcryptjs

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import prisma from "../../prisma/client";
 import jsonwebtoken from 'jsonwebtoken';
+import bcrypt from 'bcryptjs'
 
 export async function login(req: Request<{}, {}, { username: string, password: string }>, res: Response) {
     const { username, password } = req.body;
@@ -25,7 +26,7 @@ export async function login(req: Request<{}, {}, { username: string, password: s
         }
     })
 
-    if (!user || user.password !== password) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
         res.status(401).json("Username or Password are invalid. Please try again")
     } else {
         const SECURITY_TOKEN = process.env.SECURITY_TOKEN;
@@ -45,15 +46,20 @@ export async function signup(req: Request<{}, {}, { firstName: string, lastName:
     const { firstName, lastName, username, password } = req.body;
 
     try {
+        const salt = (await bcrypt.genSalt(10)).toString();
+        const hashedPassword = (await bcrypt.hash(password, salt)).toString();
+
         const user = await prisma.user.create({
-            data: { firstName, lastName, username, password }
+            data: { firstName, lastName, username, password: hashedPassword }
         });
 
         const SECURITY_TOKEN = process.env.SECURITY_TOKEN;
+
         if (!SECURITY_TOKEN) {
             console.error('Security token not found in .env')
             return res.status(500).json('Error on server side occurred')
         }
+
         const token = jsonwebtoken.sign(user, SECURITY_TOKEN)
         res.status(201).json({
             accessToken: token,
